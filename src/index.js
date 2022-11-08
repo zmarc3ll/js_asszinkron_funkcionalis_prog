2d98fe2aa5c9bfc9da0ebbebaa75310eb1f778f8
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-11-08 10:27:30 +0100

    tablazat kesz

--- src/index.js
+++ src/index.js
@@ -21,17 +21,30 @@ async function elerhetoseg() {
 
     let response = await fetch('./users.json');
     let eredmeny = await response.json();
-    let adatok = eredmeny.users;
+    let adatok = eredmeny.users.sort(function (a, b) {
+        if (a.firstName < b.firstName) { return -1; }
+        if (a.firstName > b.firstName) { return 1; }
+        return 0;
+    });
     let adatokTablazat = document.getElementById('adatTablazat');
     adatokTablazat.textContent='';
 
     for (let u of adatok) {
-        
+        let tr = document.createElement('tr');
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+        td1.innerHTML = u.username+'<br>';
+        td2.innerHTML = u.email+'<br>';
+        td3.innerHTML = u.phone+'<br>';
+        adatokTablazat.appendChild(tr);
+        adatokTablazat.appendChild(td1);
+        adatokTablazat.appendChild(td2);
+        adatokTablazat.appendChild(td3); 
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('usersButton').addEventListener('click', () => { osszesNev() });
-
-
+    document.getElementById('tableButton').addEventListener('click',() => {elerhetoseg()});
 })
